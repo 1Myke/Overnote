9b998c222fe95a019f1350b92d9f1200598c9c04
Jorjai <jorjai@jorjai.net>
2025-03-27 11:41:07 +0100

    [FIX] Update assertion in DummyTest to use JUnit Assertions

--- src/test/java/DummyTest.java
+++ src/test/java/DummyTest.java
@@ -1,9 +1,10 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class DummyTest {
     // This is a dummy test to make sure the test suite runs
     @Test
     public void test() {
-        assert true;
+        Assertions.assertTrue(true);
     }
 }
