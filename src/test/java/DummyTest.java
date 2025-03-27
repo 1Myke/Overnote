4a89d928dff00dd861f83142d8656109710ac5c1
Jorjai <jorjai@jorjai.net>
2025-03-27 11:29:54 +0100

    [ADD] Create DummyTest to check workflows

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+    // This is a dummy test to make sure the test suite runs
+    @Test
+    public void test() {
+        assert true;
+    }
+}
