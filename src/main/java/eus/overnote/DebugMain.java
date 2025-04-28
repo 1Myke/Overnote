0ed0130defe4fab777345c1de218448d0fb9b791
Jorjai <jorjai@jorjai.net>
2025-04-28 17:31:03 +0200

    [DEBUG] Fix DebugMain login issue, add locale emulation

--- src/main/java/eus/overnote/DebugMain.java
+++ src/main/java/eus/overnote/DebugMain.java
@@ -4,13 +4,18 @@ import eus.overnote.businesslogic.BlInterface;
 import eus.overnote.businesslogic.BusinessLogic;
 import javafx.application.Application;
 
+import java.util.Locale;
+
 public class DebugMain extends Application {
 
     @Override
     public void start(javafx.stage.Stage stage) {
+        // Set the locale for debugging
+        Locale.setDefault(new Locale("es", "ES"));
+
         BlInterface bl = BusinessLogic.getInstance();
-        bl.registerUser("Debug User", "debug@overnote.eus", "debug", "debug");
-        bl.loginUser("debug@overnote.eus", "debug", true);
+        bl.registerUser("Debug User", "debug@overnote.eus", "Debug12345?", "Debug12345?");
+        bl.loginUser("debug@overnote.eus", "Debug12345?", true);
         eus.overnote.presentation.WindowManager.getInstance().navigateToMain();
     }
 
